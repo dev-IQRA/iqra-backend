28cc7f07f294bfe30591131c79e66a94b6f68df7
Aldi Pramudya <llenndesu@gmail.com>
2025-05-04 15:48:29 +0700

    feat: implemented jwt passport strategy

---
+++ src/strategies/jwt-strategy.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
+const prisma = require("../prisma");
+const passport = require("passport");
+
+const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_TOKEN_SECRET,
+};
+
+passport.use(
+    new JwtStrategy(opts, async (jwt_payload, done) => {
+        try {
+            const user = await prisma.user.findUnique({ where: { id: jwt_payload.id } });
+            if (user) return done(null, user);
+            return done(null, false);
+        } catch (err) {
+            return done(err, false);
+        }
+    })
+);
+
+module.exports = passport;
