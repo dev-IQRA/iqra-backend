2203b3d19937e313fa1c3b68fced256850490d43
Aldi Pramudya <llenndesu@gmail.com>
2025-05-04 16:22:54 +0700

    fix: passport strategy fixed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { json } = require("express");
 const cookieParser = require("cookie-parser");
+const passport = require("./strategies/jwt-strategy");
+
 require("dotenv").config();
 
 const routes = require("./routes/index.js");
@@ -9,6 +11,7 @@ const app = express();
 //middlewares
 app.use(json());
 app.use(cookieParser(process.env.SECRET_KEY_SESSION));
+app.use(passport.initialize());
 app.use(routes);
 
 module.exports = app;
