958231da14a0a76dbfad37887f865b30e27f9aa8
Aldi Pramudya <llenndesu@gmail.com>
2025-05-04 19:49:25 +0700

    fix: Use cryptographically secure random number generation

--- src/utils/randomUtils.js
+++ src/utils/randomUtils.js
@@ -5,7 +5,7 @@ const generateRandomString = (length) => {
 };
 
 const generateRandomNumber = (min, max) => {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+	return crypto.randomInt(min, max + 1);
 };
 
 const generateRandomUUID = () => {
