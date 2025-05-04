b277b5b3deb7ca6a60c1999022dc3b577cbaf153
Aldi Pramudya <llenndesu@gmail.com>
2025-05-04 15:51:36 +0700

    chore: index route updated

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,11 @@
 const { Router } = require("express");
+const authRouter = require("./auth");
+const adminRouter = require("./admin");
 
 const router = Router();
+
+router.use(authRouter);
+router.use(adminRouter);
 
 router.get("/", (req, res) => {
     res.status(200).send("OK");
