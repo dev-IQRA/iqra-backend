205b703f6e29101881f4076cd9e78c59fd7524c0
Aldi Pramudya <llenndesu@gmail.com>
2025-05-04 20:00:51 +0700

    fix: ratelimiter now max 5

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -3,8 +3,9 @@ const rateLimit = require("express-rate-limit");
 /** @type {RequestHandler} */
 const authLimiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per windowMs
+	max: 5, // Limit each IP to 100 requests per windowMs
 	message: "Too many requests from this IP, please try again later.",
+	skipSuccessfulRequests: true,
 });
 
 module.exports = authLimiter;
