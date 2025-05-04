15d31538240d08d04b25541833a68f62e23985e2
Aldi Pramudya <llenndesu@gmail.com>
2025-05-04 20:47:05 +0700

    restyle

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,7 +3,7 @@ const { login } = require("../controllers/auth.controller");
 const { loginSchema } = require("../validators/authValidator");
 const validateRequest = require("../middleware/validateRequest");
 const authLimiter = require("../middleware/rateLimiter");
-const {preventLoginIfLoggedIn} = require("../middleware/authMiddleware")
+const { preventLoginIfLoggedIn } = require("../middleware/authMiddleware");
 const authRouter = Router();
 
 authRouter.post(
