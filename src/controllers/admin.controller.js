826eb3bb14bb00d4ff7c907f4b1baba247aaa357
Aldi Pramudya <llenndesu@gmail.com>
2025-05-04 15:51:18 +0700

    feat: admin system, still not implementing login admin

---
+++ src/controllers/admin.controller.js
@@ -0,0 +1,23 @@
+const { findUserByEmail, createUser } = require("../services/userService");
+const { hashPassword } = require("../utils/passwordUtils");
+const handleError = require("../utils/errorHandler");
+
+const registerUser = async (req, res) => {
+    const { email, password, role } = req.body;
+
+    try {
+        const existingUser = await findUserByEmail(email);
+        if (existingUser) {
+            return res.status(409).json({ message: "User already exists" });
+        }
+
+        const hash = await hashPassword(password);
+        const newUser = await createUser({ email, hash, role });
+
+        res.status(201).json({ message: "User registered successfully", user: newUser });
+    } catch (error) {
+        handleError(res, error);
+    }
+};
+
+module.exports = { registerUser };
