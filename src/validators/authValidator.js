a786e61c6d133407874653be8934439e91765c34
Aldi Pramudya <llenndesu@gmail.com>
2025-05-04 19:46:59 +0700

    fix: added messages to authValidator.js

--- src/validators/authValidator.js
+++ src/validators/authValidator.js
@@ -1,8 +1,14 @@
 const Joi = require("joi");
 
 const loginSchema = Joi.object({
-	email: Joi.string().email().required(),
-	password: Joi.string().min(8).required(),
+	email: Joi.string().email().required().messages({
+		"string.email": "Email must be a valid email address.",
+		"any.required": "Email is required.",
+	}),
+	password: Joi.string().min(8).required().messages({
+		"string.min": "Password must be at least 8 characters long.",
+		"any.required": "Password is required.",
+	}),
 });
 
 module.exports = { loginSchema };
