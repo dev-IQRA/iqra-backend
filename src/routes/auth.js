d0a057269ae77afc904bcf09e000a488c5a48031
Aldi Pramudya <llenndesu@gmail.com>
2025-05-04 19:41:09 +0700

    feat: applied ratelimiter on login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,9 +2,9 @@ const { Router } = require("express");
 const { login } = require("../controllers/auth.controller");
 const { loginSchema } = require("../validators/authValidator");
 const validateRequest = require("../middleware/validateRequest");
-
+const authLimiter = require("../middleware/rateLimiter")
 const authRouter = Router();
 
-authRouter.post("/api/auth/login", validateRequest(loginSchema), login);
+authRouter.post("/api/auth/login",authLimiter, validateRequest(loginSchema), login);
 
 module.exports = authRouter;
