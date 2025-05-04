8cc1b4da741666972fa2343f2b101caa17fc95e7
Aldi Pramudya <llenndesu@gmail.com>
2025-05-04 16:22:41 +0700

    fix: fixed registration fields, now handled by joi

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const { registerUser } = require("../controllers/admin.controller");
 const { authenticate, authorizeAdmin } = require("../middleware/authMiddleware.js");
-
+const validateRequest = require("../middleware/validateRequest");
+const {registerSchema} = require("../validators/registerValidator")
 const adminRouter = Router();
 
-adminRouter.post("/api/admin/register", authenticate, authorizeAdmin, registerUser);
+adminRouter.post("/api/admin/register", validateRequest(registerSchema),authenticate, authorizeAdmin, registerUser);
 
 module.exports = adminRouter;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,11 +1,13 @@
 const prisma = require("../prisma.js");
+const {generateRandomUUID} = require("../utils/randomUtils");
 
 const findUserByEmail = async (email) => {
     return prisma.user.findUnique({ where: { email } });
 };
 
 const createUser = async (data) => {
-    return prisma.user.create({ data });
+    const gen_id = generateRandomUUID();
+    return prisma.user.create({ data: { id: gen_id, ...data } });
 };
 
 module.exports = { findUserByEmail, createUser };
