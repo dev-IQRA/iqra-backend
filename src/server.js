ba6a58e8def6065999e5b02745cff806dcb9f97c
Aldi Pramudya <llenndesu@gmail.com>
2025-05-04 20:53:14 +0700

    Revert "Update src/server.js"
    This reverts commit 56caf8b20d3e955545b581e556312212fda7aa9f.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
 const app = require("./app.js");
 
---- README.md
-@@ -1,1 +1,1
-- CORS_ORIGIN=http://localhost:3000
-+ CORS_ORIGIN=http://localhost:3333
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
