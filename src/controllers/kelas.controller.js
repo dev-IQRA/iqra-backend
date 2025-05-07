6c65adaee374eeb8fdda83b9699218275410a6a0
Aldi Pramudya <llenndesu@gmail.com>
2025-05-07 23:34:09 +0700

    feat: still in beta, creating kelas and querying it

---
+++ src/controllers/kelas.controller.js
@@ -0,0 +1,37 @@
+const {viewAllKelas ,findKelasById, createKelas} = require("../services/kelasService");
+const handleError = require("../utils/errorHandler");
+
+const createNewKelas = async (req, res) => {
+    const {id, nama_kelas, tingkat} = req.body;
+
+    try {
+        const existingKelas = await findKelasById(id);
+        if (existingKelas) return res.status(409).json({message: "This kelas already existed."});
+
+        const result = await createKelas({id, nama_kelas, tingkat});
+        return res.status(200).json({ result });
+    } catch (err) {
+        handleError(res, err);
+    }
+}
+
+const viewKelas = async (req, res) => {
+    try {
+        const result = await viewAllKelas();
+        return res.status(200).json({ result });
+    } catch (err) {
+        handleError(res, err);
+    }
+}
+
+const viewKelasById = async (req, res) => {
+    try {
+
+        const result = await findKelasById();
+        return res.status(200).json({ result });
+    } catch (err) {
+        handleError(res, err);
+    }
+}
+
+module.exports = {viewKelasById,viewKelas,createNewKelas}
---
+++ src/routes/kelas.js
@@ -0,0 +1,25 @@
+const { Router } = require("express");
+const { createNewKelas } = require("../controllers/kelas.controller");
+const {
+    authenticate,
+    authorizeAdmin,
+} = require("../middleware/authMiddleware.js");
+const validateRequest = require("../middleware/validateRequest");
+const { kelasSchema } = require("../validators/authValidator");
+const kelasRouter = Router();
+
+kelasRouter.post(
+    "/api/admin/kelas",
+    authenticate,
+    authorizeAdmin,
+    validateRequest(kelasSchema),
+    createNewKelas,
+);
+
+kelasRouter.get(
+    "/api/admin/kelas",
+    authenticate,
+    //viewKelas,
+);
+
+module.exports = kelasRouter;
