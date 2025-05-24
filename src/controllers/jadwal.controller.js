d25c7e1de4af9b9a9246690ad5c6c96a2f8a2798
Akbar3776 <akbar3776@gmail.com>
2025-05-24 16:24:42 +0700

    feat: controller for jadwal

---
+++ src/controllers/jadwal.controller.js
@@ -0,0 +1,94 @@
+const jadwalSchema = require("../validators/jadwalValidator");
+const {
+  getAllJadwal,
+  createJadwal,
+  getJadwalById,
+  updateJadwal,
+  deleteJadwal
+} = require("../services/jadwalService");
+const { formatTo24Hour } = require("../utils/timeFormatter");
+const handleError = require("../utils/errorHandler");
+
+const viewAllJadwal = async (req, res) => {
+  try {
+    const jadwal = await getAllJadwal();
+    if (!jadwal || jadwal.length === 0)
+      return res.status(404).json({ message: "Tidak ada jadwal ditemukan." });
+
+    // Perbarui pemanggilan fungsi agar menggunakan formatTo24Hour
+    const formattedJadwal = jadwal.map((j) => ({
+      ...j,
+      jam_mulai: formatTo24Hour(j.jam_mulai),
+      jam_selesai: formatTo24Hour(j.jam_selesai)
+    }));
+
+    res.status(200).json({ jadwal: formattedJadwal });
+  } catch (err) {
+    handleError(res, err);
+  }
+};
+
+
+const addJadwal = async (req, res) => {
+  const { error } = jadwalSchema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  try {
+    const jadwal = await createJadwal(req.body);
+    res.status(201).json({ jadwal });
+  } catch (err) {
+    handleError(res, err);
+  }
+};
+
+const getJadwalDetail = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const jadwal = await getJadwalById(id);
+    if (!jadwal)
+      return res.status(404).json({ message: "Jadwal tidak ditemukan." });
+
+    // Format jam_mulai dan jam_selesai ke format 24 jam
+    const formattedJadwal = {
+      ...jadwal,
+      jam_mulai: formatTo24Hour(jadwal.jam_mulai),
+      jam_selesai: formatTo24Hour(jadwal.jam_selesai),
+    };
+
+    res.status(200).json({ jadwal: formattedJadwal });
+  } catch (err) {
+    handleError(res, err);
+  }
+};
+
+const updateJadwalData = async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  try {
+    const jadwal = await updateJadwal(id, data);
+    res.status(200).json({ jadwal });
+  } catch (err) {
+    handleError(res, err);
+  }
+};
+
+const deleteJadwalData = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await deleteJadwal(id);
+    res.status(200).json({ message: "Jadwal berhasil dihapus." });
+  } catch (err) {
+    handleError(res, err);
+  }
+};
+
+module.exports = {
+  viewAllJadwal,
+  addJadwal,
+  getJadwalDetail,
+  updateJadwalData,
+  deleteJadwalData
+};
